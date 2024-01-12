c87c95a1b86971813e46340272eeb64fb88f38e5
rouali <rouali@student.1337.ma>
2024-01-12 19:47:40 +0100

    Update Config CGI Env

---
+++ cgi/uploads.php
@@ -0,0 +1,22 @@
+<?php
+$folderPath = '../html_root/uploads';
+
+$images = glob($folderPath . '/*.{jpg,jpeg,png,gif,svg}', GLOB_BRACE);
+
+if (count($images) > 0)
+{
+    echo "<h1>Uploaded Images</h1>";
+    echo count($images);
+    echo '<br>';
+    foreach ($images as $image)
+    {
+        $imageName = basename($image);
+        $imageUrl = "./$imageName";
+        echo "<a href='/$imageUrl'>$imageName</a><br>";
+    }
+}
+else
+{
+    echo "<p>No images found in the specified folder.</p>";
+}
+?>
