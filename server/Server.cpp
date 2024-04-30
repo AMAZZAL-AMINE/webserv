8142a9add051b18239099f9b05facbc6154d929b
Youssef Ichiba <yichiba@c1r2p5.1337.ma>
2024-04-30 12:46:51 +0100

    hahia salat

--- server/Server.cpp
+++ server/Server.cpp
@@ -63,7 +63,7 @@ void    Server::setup_clients(fd_set& readFd, int &max, std::map<int, int> & cli
                 continue;
             }
             std::cout << "NEW CLIENT CONNECTED : " << clientFd << "\n";
-            set_nonblock(clientFd);
+            // set_nonblock(clientFd);
             t_response response;
             response.client_fd = clientFd;
             response.server_fd = *it;
